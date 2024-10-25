462630440c9acf5d4f7a7cb3139f7b69e8010bf6
Badyss <badyss.blt@gmail.com>
2024-10-25 14:38:42 +0200

    feat: scrolling area

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import {FlatList, Image, StyleSheet, Text, TextInput, View} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import { FlatList, StyleSheet, TextInput, View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import ThemedText from "@/components/ThemedText";
-import {useThemeColors} from "@/hooks/useThemeColors";
+import { useThemeColors } from "@/hooks/useThemeColors";
 import StatsCard from "@/components/StatsCard";
-import {useState} from "react";
+import { useState } from "react";
 import FoodCard from "@/components/FoodCard";
 
 type FoodType = {
@@ -14,13 +14,11 @@ type FoodType = {
         color: string,
         title: string
     }[];
-    image?: string
+    image?: string;
 };
 
 export default function Index() {
-
     const [searchText, setSearchText] = useState("");
-
     const colors = useThemeColors();
 
     const [foods, setFoods] = useState<FoodType[]>([
@@ -29,14 +27,8 @@ export default function Index() {
             quantity: 4,
             expiredAt: "27/10/2024",
             type: [
-                {
-                    title: "Légumé",
-                    color: "#20BF55"
-                },
-                {
-                    title: "Sain",
-                    color: "#20BF55"
-                }
+                { title: "Légumé", color: "#20BF55" },
+                { title: "Sain", color: "#20BF55" }
             ],
             image: "https://www.primeale.fr/app/uploads/2022/03/primeale-les-carottes-vrac.jpg"
         },
@@ -45,14 +37,8 @@ export default function Index() {
             quantity: 4,
             expiredAt: "27/10/2024",
             type: [
-                {
-                    title: "Légumé",
-                    color: "#20BF55"
-                },
-                {
-                    title: "Sain",
-                    color: "#20BF55"
-                }
+                { title: "Légumé", color: "#20BF55" },
+                { title: "Sain", color: "#20BF55" }
             ]
         },
         {
@@ -60,14 +46,8 @@ export default function Index() {
             quantity: 4,
             expiredAt: "27/10/2024",
             type: [
-                {
-                    title: "Légumé",
-                    color: "#20BF55"
-                },
-                {
-                    title: "Sain",
-                    color: "#20BF55"
-                }
+                { title: "Légumé", color: "#20BF55" },
+                { title: "Sain", color: "#20BF55" }
             ]
         },
         {
@@ -75,57 +55,52 @@ export default function Index() {
             quantity: 4,
             expiredAt: "27/10/2024",
             type: [
-                {
-                    title: "Légumé",
-                    color: "#20BF55"
-                },
-                {
-                    title: "Sain",
-                    color: "#20BF55"
-                }
+                { title: "Légumé", color: "#20BF55" },
+                { title: "Sain", color: "#20BF55" }
             ]
         },
-
     ]);
 
     const filteredFoods = foods.filter(food =>
         food.name.toLowerCase().includes(searchText.toLowerCase())
     );
 
-
     return (
-        <SafeAreaView style={{flex: 1}}>
+        <SafeAreaView>
             {/* Top container */}
-            <View style={[style.topContainer, {backgroundColor: colors.tint}]}>
-                <ThemedText color="white" variant="title">Bonjour, John Doe</ThemedText>
-                <ThemedText color="white60">Que voulez-vous faire aujourd'hui ?</ThemedText>
-            </View>
-
-            {/* Mes aliments */}
-            <View style={[style.bodyContainer, {flex: 1}]}>
-                <ThemedText variant="title" styles={{fontWeight: "bold"}}>Mes aliments</ThemedText>
-                <View style={style.statContainer}>
-                    <StatsCard title="Total d'aliments" number={foods.length} image={require('@/assets/images/carrot.png')} />
-                    <StatsCard title="Total d'aliments" number={foods.length} image={require('@/assets/images/carrot.png')} />
+            <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
+                <View style={[style.topContainer, { backgroundColor: colors.tint }]}>
+                    <ThemedText color="white" variant="title">Bonjour, John Doe</ThemedText>
+                    <ThemedText color="white60">Que voulez-vous faire aujourd'hui ?</ThemedText>
                 </View>
 
-                {/* Rechercher */}
-                <View style={style.inputContainer}>
-                    <ThemedText>Rechercher</ThemedText>
-                    <TextInput placeholder="Pomme de terre" style={style.input} value={searchText} onChangeText={setSearchText} />
-                </View>
+                {/* Mes aliments */}
+                <View style={[style.bodyContainer]}>
+                    <ThemedText variant="title" styles={{ fontWeight: "bold" }}>Mes aliments</ThemedText>
+                    <View style={style.statContainer}>
+                        <StatsCard title="Total d'aliments" number={foods.length} image={require('@/assets/images/carrot.png')} />
+                        <StatsCard title="Total d'aliments" number={foods.length} image={require('@/assets/images/carrot.png')} />
+                    </View>
 
-                {/* Liste des aliments */}
-                <FlatList
-                    data={filteredFoods}
-                    numColumns={1}
-                    contentContainerStyle={{gap: 10, paddingBottom: 20, marginTop: 10}}
-                    renderItem={({ item }) => (
-                        <FoodCard item={item}  />
-                    )}
-                    keyExtractor={(item) => item.name}
-                />
-            </View>
+                    {/* Rechercher */}
+                    <View style={style.inputContainer}>
+                        <ThemedText>Rechercher</ThemedText>
+                        <TextInput placeholder="Pomme de terre" style={style.input} value={searchText} onChangeText={setSearchText} />
+                    </View>
+
+                    {/* Liste des aliments */}
+                    <FlatList
+                        data={filteredFoods}
+                        numColumns={1}
+                        contentContainerStyle={{ gap: 10, marginTop: 10 }}
+                        renderItem={({ item }) => (
+                            <FoodCard item={item} />
+                        )}
+                        keyExtractor={(item) => item.name}
+                        scrollEnabled={false}  // Disable FlatList scrolling
+                    />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
@@ -154,5 +129,4 @@ const style = StyleSheet.create({
         borderRadius: 5,
         padding: 5
     }
-})
-
+});
